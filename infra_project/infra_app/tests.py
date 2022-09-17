e075f403ef12476c6901e5f5d9fc1c66ae3e3d05
mstee1 <astafiev.maksim@yandex.ru>
2022-09-17 13:24:55 +0300

    3 fix

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -9,16 +9,16 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('')
+        response = self.guest_client.get('http://127.0.0.1:8000')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('http://127.0.0.1:8000/second/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('')
+        response = self.guest_client.get('http://127.0.0.1:8000')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('second/')
+        response = self.guest_client.get('http://127.0.0.1:8000/second/')
         self.assertContains(response, 'А это вторая страница!')
